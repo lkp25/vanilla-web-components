d5248477133d3c2ff278e222c6ac4f428b1a2c56
lkp25 <lkp25@o2.pl>
2021-08-09 12:37:27 +0200

    using templates and shadow dom and slot property

--- tooltip.js
+++ tooltip.js
@@ -2,24 +2,36 @@ class Tooltip extends HTMLElement{
     constructor(){
         super()
         this._tooltipContainer
+        this._tooltipText = "some dummy text..."
         
+        //attach shadowDOM
+        this.attachShadow({mode: "open"})
+        //get the html template defined in the view file:
+        const template = document.querySelector('#mytemp')
+        //deep clone the template content INTO SHADOW DOM
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
     }
     connectedCallback(){
-        const tooltipIcon = document.createElement('span')
-        tooltipIcon.textContent = "(?)"
+        //get the text attr value for use in the custom tooltip:
+        if(this.hasAttribute('text')){//only use the attr text if it was added (user might not add it!)
+            this._tooltipText = this.getAttribute('text')
+        }
+        const tooltipIcon = this.shadowRoot.querySelector('span')
+       
         //hovering on element listener- bind this to the class instance (the HTMLELEMENT) and not to the mouseenter EVENT!
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
-        this.appendChild(tooltipIcon)
+        this.shadowRoot.appendChild(tooltipIcon)
     }
     //method to be called on hover
     _showTooltip(){
         this._tooltipContainer = document.createElement('div')
-        this._tooltipContainer.innerText = "this is a ext of tooltip"
-        this.appendChild(this._tooltipContainer)
+        this._tooltipContainer.innerText = this._tooltipText
+        this.shadowRoot.appendChild(this._tooltipContainer)
     }
     _hideTooltip(){
         this._tooltipContainer.remove()
+        // this.shadowRoot.removeChild(this._tooltipContainer) -- same effect
     }
 }
 customElements.define('lkp-tooltip', Tooltip)
